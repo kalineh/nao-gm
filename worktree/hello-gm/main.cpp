281b2e7867baa59ffa9051d3598e33c6fab597ff
kalineh <kalin.eh@gmail.com>
2012-07-22 11:56:51 +0900

    naoqi-sdk + gm available

--- worktree/hello-gm/main.cpp
+++ worktree/hello-gm/main.cpp
@@ -3,7 +3,8 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the COPYING file.
  */
-#include <iostream>
+
+#include <cstdlib>
 
 #include <alcommon/alproxy.h>
 
@@ -13,8 +14,17 @@ void RegisterProjectLibs(gmMachine* vm)
 {
 }
 
-int main()
+int main(int argc, char** argv)
 {
-    std::pro
+    std::string ip = "192.168.11.9";
+    std::string port = "9559";
+
+    if (argc > 1) ip = argv[1];
+    if (argc > 2) port = argv[2];
+
+    AL::ALProxy* proxy = new AL::ALProxy("ALTextToSpeech", ip, std::atoi(port.c_str()));
+
+    proxy->callVoid("say", std::string("hello"));
+
     return 0;
 }
--- worktree/hello/main.cpp
+++ worktree/hello/main.cpp
@@ -5,9 +5,13 @@
  */
 #include <iostream>
 #include <alcommon/alproxy.h>
+#include <alproxies/altexttospeechproxy.h>
 
 int main()
 {
   std::cout << "Hello, world" << std::endl;
+
+    AL::ALTextToSpeechProxy tts(std::string("192.168.11.9"), 9559);
+    tts.say(std::string("test"));
   return 0;
 }
