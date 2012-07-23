32f66722981b6fe22d7d3716216cf97cd776e90e
kalineh <kalin.eh@gmail.com>
2012-07-23 23:26:17 +0900

    added first gm test

--- worktree/hello-gm/main.cpp
+++ worktree/hello-gm/main.cpp
@@ -5,449 +5,115 @@
  */
 
 #include <cstdlib>
-
 #include <string>
 
-#include <alcommon/almodule.h>
-#include <alcommon/alproxy.h>
-#include <alcommon/albroker.h>
-#include <alcommon/albrokermanager.h>
-
-#include <alvalue/alvalue.h>
-#include <althread/almutex.h>
-#include <althread/alcriticalsection.h>
-
-#include <alproxies/almotionproxy.h>
-#include <alproxies/altexttospeechproxy.h>
-#include <alproxies/alspeechrecognitionproxy.h>
-#include <alproxies/almemoryproxy.h>
-
-#include <qi/log.hpp>
-#include <qi/os.hpp>
-
 #include "gm/gmMachine.h"
+#include "gm/gmBind.h"
 
+#include "Core.h"
+#include <common/StrongHandle.h>
+#include <common/HandledObj.h>
 
-// no exception mode
-//#define MODULE_ERROR(description) do { printf(description); qi::os::exit(1); } while (0)
+#include <alcommon/alproxy.h>
 
-#define MODULE_ERROR(description) do { throw AL::ALError(getName(), __FUNCTION__, description, __FILE__, __LINE__); } while (0)
+using namespace funk;
 
-#define ARRAY_COUNT(a) (sizeof(a) / sizeof(a[0]))
+#define ASSERT(condition) \
+    assert(false)//if (!condition) { __asm { int 3; } }
+
+class GMALProxy
+    : public HandledObj<GMALProxy>
+{
+public:
+	GM_BIND_TYPEID(GMALProxy);
+
+    GMALProxy(const char* type, const char* ip, int port)
+        : _proxy(std::string(type), std::string(ip), port)
+    {
+    }
+
+    void CallVoid(const char* function, gmVariable arg)
+    {
+        const std::string function_string = std::string(function);
+
+        int result = -1;
+
+        try
+        {
+            switch (arg.m_type)
+            {
+            case GM_INT: _proxy.callVoid(function_string, arg.GetInt()); break;
+            case GM_FLOAT: _proxy.callVoid(function_string, arg.GetFloat()); break;
+            case GM_VEC2: _proxy.callVoid(function_string, arg.GetVec2().x, arg.GetVec2().y); break;
+            case GM_STRING: _proxy.callVoid(function_string, std::string(arg.GetCStringSafe())); break;
+            default: _proxy.callVoid(function_string); break;
+            }
+        }
+        catch (const AL::ALError& e)
+        {
+            printf("error: %s\n", e.what());
+        }
+    }
+
+private:
+    AL::ALProxy _proxy;
+};
+
+GM_BIND_DECL(GMALProxy);
+
+GM_REG_NAMESPACE(GMALProxy)
+{
+	GM_MEMFUNC_DECL(CreateGMALProxy)
+	{
+		GM_CHECK_NUM_PARAMS(3);
+        GM_CHECK_STRING_PARAM(type, 0);
+        GM_CHECK_STRING_PARAM(ip, 1);
+        GM_CHECK_INT_PARAM(port, 2);
+		GM_PUSH_USER_HANDLED( GMALProxy, new GMALProxy(type, ip, port) );
+		return GM_OK;
+	}
+
+    GM_MEMFUNC_DECL(CallVoid)
+    {
+        GM_CHECK_NUM_PARAMS(1);
+        GM_CHECK_STRING_PARAM(function, 0);
+        //GM_CHECK_STRING_PARAM(str, 1);
+
+		GM_GET_THIS_PTR(GMALProxy, self);
+		self->CallVoid(function, a_thread->Param(1));
+        return GM_OK;
+    }
+}
+
+GM_REG_MEM_BEGIN(GMALProxy)
+GM_REG_MEMFUNC( GMALProxy, CallVoid )
+//GM_REG_MEMFUNC( Cam2d, End )
+//GM_REG_MEMFUNC( Cam2d, InitScreenSpace )
+//GM_REG_MEMFUNC( Cam2d, InitScreenSpaceSize )
+//GM_REG_MEMFUNC( Cam2d, SetBounds )
+//GM_REG_MEMFUNC( Cam2d, GetPos )
+//GM_REG_MEMFUNC( Cam2d, SetPos )
+//GM_REG_MEMFUNC( Cam2d, SetLookAt )
+//GM_REG_MEMFUNC( Cam2d, SetNearFar )
+//GM_REG_HANDLED_DESTRUCTORS(Cam2d)
+GM_REG_MEM_END()
+
+GM_BIND_DEFINE(GMALProxy);
 
 
 void RegisterProjectLibs(gmMachine* vm)
 {
+	GM_BIND_INIT( GMALProxy, vm );
 }
-
-void ListenForSound()
-{
-}
-
-class HandVoiceControl
-    : public AL::ALModule
-{
-public:
-    HandVoiceControl(AL::ALBroker::Ptr broker, const std::string& name)
-        : AL::ALModule(broker, std::string("HandVoiceControl"))
-        , mutex(AL::ALMutex::createALMutex())
-    {
-        setModuleDescription("Control hand open/close with voice commands.");
-
-        functionName("onWordRecognized", getName(), "Handle voice command notifications.");
-        BIND_METHOD(HandVoiceControl::onWordRecognized);
-    }
-
-    virtual ~HandVoiceControl()
-    {
-    }
-
-    void startRecognition()
-    {
-        try
-        {
-            memory = AL::ALMemoryProxy(getParentBroker());
-            speech = AL::ALTextToSpeechProxy(getParentBroker());
-            recognition = AL::ALSpeechRecognitionProxy(getParentBroker());
-
-#define IGNORE_EXCEPTION(code) try { code ; } catch (...) { }
-               
-            IGNORE_EXCEPTION((memory.unsubscribeToEvent("SpeechDetected", getName())));
-            IGNORE_EXCEPTION((memory.unsubscribeToEvent("WordRecognized", getName())));
-
-#undef IGNORE_EXCEPTION
-
-            // force an un-subscribe in case we are already on (this stops the ASR)
-            //memory.unsubscribeToEvent("WordRecognized", getName());
-            //recognition.unsubscribe(getName());
-
-            recognition.setAudioExpression(true);
-            recognition.setVisualExpression(true);
-            //recognition.setParameter("EarSpeed", 0.0f);
-            //recognition.setParameter("EarUseSpeechDetector", 2.0f);
-            //recognition.setParameter("EarUseFilter", 1.0f);
-
-            std::vector<std::string> vocabulary;
-
-            vocabulary.push_back("open");
-            vocabulary.push_back("close");
-
-            recognition.setVocabulary(vocabulary, true);
-            recognition.setLanguage("English");
-
-            memory.subscribeToEvent("WordRecognized", getName(), "onWordRecognized");
-            //memory.subscribeToEvent("SpeechDetected", getName(), "onWordRecognized");
-
-            speech.say("started listening");
-        }
-        catch (const AL::ALError& e)
-        {
-            qiLogError("HandVoiceControl") << e.what() << std::endl;
-        }
-    }
-
-    void stopRecognition()
-    {
-        speech.say("stopped listening");
-        memory.unsubscribeToEvent("onWordRecognized", "HandleVoiceControl");
-    }
-
-    virtual void init()
-    {
-    }
-
-    struct WordConfidencePair
-    {
-        std::string word;
-        float confidence;
-    };
-
-    struct WordConfidencePairComparison
-    {
-        bool operator()(const WordConfidencePair& lhs, const WordConfidencePair& rhs)
-        {
-            return lhs.confidence < rhs.confidence;
-        }
-    };
-
-    void ParseWordRecognizedArray(std::vector<WordConfidencePair>& results, const AL::ALValue& value, float recognitionThreshold)
-    {
-        // unexpected data
-        if (value.getType() != AL::ALValue::TypeArray)
-            MODULE_ERROR("invalid array type");
-
-        // unexpected data
-        if (value.getSize() % 2 != 0)
-            MODULE_ERROR("invalid array size");
-
-        for (int i = 0; i < (int)value.getSize(); i += 2)
-        {
-            AL::ALValue wordValue = value[i];
-            AL::ALValue confidenceValue = value[i + 1];
-
-            float confidence = confidenceValue.operator float &();
-            if (confidence >= recognitionThreshold)
-            {
-                WordConfidencePair pair = { wordValue.toString(), confidence };
-                results.push_back(pair);
-            }
-        }
-
-        std::sort(results.begin(), results.end(), WordConfidencePairComparison());
-    }
-
-    void onWordRecognized(const std::string& name, const AL::ALValue& value, const std::string& myname)
-    {
-        try
-        {
-            AL::ALCriticalSection section(mutex);
-            AL::ALValue v = memory.getData("WordRecognized");
-
-            std::vector<WordConfidencePair> words;
-            ParseWordRecognizedArray(words, value, 0.5f);
-
-            if (words.size() > 0)
-            {
-                speech.say(words[0].word);
-            }
-
-            //speech.say("word");
-            //speech.say(v.toString());
-        }
-        catch (const AL::ALError& e)
-        {
-            qiLogError("onWordRecognized") << e.what() << std::endl;
-        }
-
-        // this gets called a lot, try and suppress excessive calls
-        qi::os::msleep(2000);
-    }
-
-private:
-    AL::ALMemoryProxy memory;
-    AL::ALTextToSpeechProxy speech;
-    AL::ALSpeechRecognitionProxy recognition;
-
-    boost::shared_ptr<AL::ALMutex> mutex;
-};
-
-class Diagnostics
-    : public AL::ALModule
-{
-public:
-    Diagnostics(AL::ALBroker::Ptr broker, const std::string& name)
-        : AL::ALModule(broker, std::string("Diagnostics"))
-        , mutex(AL::ALMutex::createALMutex())
-    {
-        setModuleDescription("Report various diagnostics.");
-    }
-
-    struct BodyPartDescriptor
-    {
-        const char* name;
-        const char* desc;
-    };
-
-    static const BodyPartDescriptor BodyParts[26];
-
-    int getBodyPartCount()
-    {
-        return sizeof(BodyParts) / sizeof(BodyParts[0]);
-    }
-
-    void reportAllBodyParts()
-    {
-        for (int i = 0; i < getBodyPartCount(); ++i)
-        {
-            reportBodyPartName(i);
-        }
-    }
-
-    void reportStatus()
-    {
-        // TODO: is there overhead in creating one of these every time?
-        AL::ALMotionProxy motion(getParentBroker());
-        AL::ALTextToSpeechProxy speech(getParentBroker());
-        speech.setLanguage("English");
-        std::string summary = motion.getSummary();
-        //speech.say(motion.getSummary());
-    }
-
-    void reportBodyPartName(int index)
-    {
-        AL::ALCriticalSection section(mutex);
-
-        BodyPartDescriptor part = Diagnostics::BodyParts[index];
-
-        // TODO: is there overhead in creating one of these every time?
-        AL::ALTextToSpeechProxy speech(getParentBroker());
-        speech.setLanguage("English");
-        speech.say(std::string(part.desc));
-    }
-
-    void reportTemperatures(float thresholdDegrees = 40.0f)
-    {
-        for (int i = 0; i < getBodyPartCount(); ++i)
-        {
-        }
-    }
-
-    virtual void init()
-    {
-    }
-
-    boost::shared_ptr<AL::ALMutex> mutex;
-};
-
-const Diagnostics::BodyPartDescriptor Diagnostics::BodyParts[26] = { 
-    { "HeadYaw", "Head Yaw", },
-    { "LShoulderPitch", "Left Shoulder Pitch", },
-    { "LHipYawPitch", "Left Hip Yaw Pitch", },
-    { "RHipYawPitch", "Right Hip Yaw Pitch", },
-    { "RShoulderPitch", "Right Shoulder Pitch", },
-    { "HeadPitch", "Head Pitch", },
-    { "LShoulderRoll", "Left Shoulder Roll", },
-    { "LHipRoll", "Left Hip Roll", },
-    { "RHipRoll", "Right Hip Roll", },
-    { "RShoulderRoll", "Right Shoulder Roll", },
-    { "LElbowYaw", "Left Elbow Yaw", },
-    { "LHipPitch", "Left Hip Pitch", },
-    { "RHipPitch", "Right Hip Pitch", },
-    { "RElbowYaw", "Right Elbow Yaw", },
-    { "LElbowRoll", "Left Elbow Roll", },
-    { "LKneePitch", "Left Knee Pitch", },
-    { "RKneePitch", "Right Knee Pitch", },
-    { "RElbowRoll", "Right Elbow Roll", },
-    { "LWristYaw", "Left Wrist Yaw", },
-    { "LAnklePitch", "Left Ankle Pitch", },
-    { "RAnklePitch", "Right Ankle Pitch", },
-    { "RWristYaw", "Right Wrist Yaw", },
-    { "LHand", "Left Hand", },
-    { "RAnkleRoll", "Right Ankle Roll", },
-    { "LAnkleRoll", "Left Angle Roll", },
-    { "RHand", "Rigth Hand", },
-};
-
-void randomtest()
-{
-    std::string ip = "192.168.11.9";
-    std::string port = "9559";
-
-    int portn = std::atoi(port.c_str());
-
-    AL::ALProxy tts("ALTextToSpeech", ip, portn);
-    tts.callVoid("say", std::string("hello"));
-
-    AL::ALSpeechRecognitionProxy sr(ip, portn);
-
-    sr.setAudioExpression(true);
-    sr.setVisualExpression(true);
-    sr.setLanguage("English");
-
-    AL::ALMotionProxy motion(ip, portn);
-    int a = motion.post.openHand("RHand");
-    tts.callVoid("say", std::string("opening"));
-    int b = motion.post.closeHand("RHand");
-    tts.callVoid("say", std::string("closing"));
-
-    while (true)
-    {
-    }
-}
-
-class Bumper : public AL::ALModule
-{
-  public:
-    Bumper(boost::shared_ptr<AL::ALBroker> broker, const std::string& name);
-    virtual ~Bumper();
-    virtual void init();
-    void onRightBumperPressed();
-
-  private:
-    AL::ALMemoryProxy fMemoryProxy;
-    AL::ALTextToSpeechProxy fTtsProxy;
-
-    boost::shared_ptr<AL::ALMutex> fCallbackMutex;
-    float fState;
-};
-
-Bumper::Bumper(
-  boost::shared_ptr<AL::ALBroker> broker,
-  const std::string& name): AL::ALModule(broker, name),
-    fCallbackMutex(AL::ALMutex::createALMutex())
-{
-  setModuleDescription("This module presents how to subscribe to a simple event (here RightBumperPressed) and use a callback method.");
-
-  functionName("onRightBumperPressed", getName(), "Method called when the right bumper is pressed. Makes a LED animation.");
-  BIND_METHOD(Bumper::onRightBumperPressed)
-}
-
-Bumper::~Bumper() {
-  fMemoryProxy.unsubscribeToEvent("onRightBumperPressed", "Bumper");
-}
-
-void Bumper::init() {
-  try {
-    /** Create a proxy to ALMemory.
-    */
-    fMemoryProxy = AL::ALMemoryProxy(getParentBroker());
-
-    fState = fMemoryProxy.getData("RightBumperPressed");
-    /** Subscribe to event LeftBumperPressed
-    * Arguments:
-    * - name of the event
-    * - name of the module to be called for the callback
-    * - name of the bound method to be called on event
-    */
-    fMemoryProxy.subscribeToEvent("RightBumperPressed", "Bumper",
-                                  "onRightBumperPressed");
-  }
-  catch (const AL::ALError& e) {
-    qiLogError("module.example") << e.what() << std::endl;
-  }
-}
-
-void Bumper::onRightBumperPressed() {
-  qiLogInfo("module.example") << "Executing callback method on right bumper event" << std::endl;
-  /**
-  * As long as this is defined, the code is thread-safe.
-  */
-  AL::ALCriticalSection section(fCallbackMutex);
-
-  /**
-  * Check that the bumper is pressed.
-  */
-  fState =  fMemoryProxy.getData("RightBumperPressed");
-  if (fState  > 0.5f) {
-    return;
-  }
-  try {
-    fTtsProxy = AL::ALTextToSpeechProxy(getParentBroker());
-    fTtsProxy.say("Right bumper pressed");
-  }
-  catch (const AL::ALError& e) {
-    qiLogError("module.example") << e.what() << std::endl;
-  }
-}
-
-#include "Core.h"
 
 int main(int argc, char** argv)
 {
 	funk::Core app;
-	//app.AddChild( new funk::AppReflection );
-	app.HandleArgs(argc,argv);
+
+	app.HandleArgs(argc, argv);
 	app.Init();
 	app.Run();
 	app.Deinit();
-
-    return 0;
-
-    std::string ip = "192.168.11.9";
-    std::string port = "9559";
-
-    if (argc > 1) ip = argv[1];
-    if (argc > 2) port = argv[2];
-
-    int portn = std::atoi(port.c_str());
-
-    AL::ALBrokerManager::getInstance()->killAllBroker();
-    AL::ALBroker::Ptr broker = AL::ALBroker::createBroker("main", "0.0.0.0", 54000, ip, portn);
-
-    try
-    {
-        auto hvc = AL::ALModule::createModule<HandVoiceControl>(broker, "HandVoiceControl");
-        hvc->startRecognition();
-
-        AL::ALMotionProxy motion(broker);
-
-        auto d = AL::ALModule::createModule<Diagnostics>(broker, "Diagnostics");
-        d->reportStatus();
-
-        static bool finish = false;
-        while (!finish)
-        {
-            motion.openHand("RHand");
-            motion.setStiffnesses("HeadYaw", 0.1f);
-            motion.angleInterpolation("HeadYaw", 0.2f, 1.0f, true);
-            qi::os::msleep(1000);
-            motion.closeHand("RHand");
-            motion.angleInterpolation("HeadYaw", 0.0f, 1.0f, true);
-            qi::os::msleep(1000);
-        }
-
-        hvc->stopRecognition();
-    }
-    catch (const AL::ALError& e)
-    {
-        qiLogError("error") << e.what() << std::endl;
-    }
-
-    //AL::ALMotionProxy motion(ip, portn);
-    //int a = motion.post.openHand("RHand");
-    //tts.callVoid("say", std::string("opening"));
-    //int b = motion.post.closeHand("RHand");
-    //tts.callVoid("say", std::string("closing"));
 
     return 0;
 }
